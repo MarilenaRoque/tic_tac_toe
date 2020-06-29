b890ea63bfdef0777318b1f0a6698478077af1c5
JasemDuncan <jasemvalencia@gmail.com>
2020-06-29 10:45:10 -0500

    Add pseudo code

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,29 @@
 #!/usr/bin/env ruby
-puts 'Hello!'
+#while the user wants to play   
+    #if it is the first time    
+    #ask player 1 name    
+    #ask player 2 name  
+#end
+
+#while there is no winner or there was less than 9 moves    
+    # print the table    
+    # ask for the player movement    
+    # call the function to test if there is a winner
+    # if there_is_a_winner?
+        #print table
+        #print player is a winner      
+        #break the while
+    # end    
+    # change the player  
+#end
+
+# if !there_is_a_winner?    
+    # print table    
+    # print "There is a draw"  
+# end
+
+# Ask user ig he want to play again  
+    # if yes run loop again  
+    # if no break the loop
+# end
 
