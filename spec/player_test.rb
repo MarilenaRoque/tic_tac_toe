f11c15c8197ef92eebfa7354d5e83a67b9772947
JasemDuncan <jasemvalencia@gmail.com>
2020-07-10 14:32:30 -0500

    Test initialize method for player class

---
+++ spec/player_test.rb
@@ -0,0 +1,15 @@
+require '../lib/player.rb'
+
+describe Player do
+    before(:all) do
+        @player=Player.new('jorge','X')        
+    end
+    describe 'initialize' do
+        it 'Testing if right name is assigned' do
+            expect(@player.name).to eq('jorge')            
+        end
+        it 'Testing if right symbol is assigned' do
+            expect(@player.symbol).to eq('X')
+        end
+    end
+end
